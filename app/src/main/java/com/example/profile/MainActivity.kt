3ebd2fd7a791f6ac2b14377263d0099d3c5e547c
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2026-07-06 23:47:09 +0200

    Accessibilité du bouton

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -70,7 +74,7 @@ fun Skills(skills: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun GitHubButton(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Button(onClick = onClick, modifier = modifier) {
+    Button(onClick = onClick, modifier = modifier.clickable(role = Role.Button, onClick = onClick)) {
         Text(text = text)
     }
 }
